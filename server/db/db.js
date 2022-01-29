7114bab774975800a8a1f732c43142ec9374f38f
Joe Alves <joseph.michael.alves@gmail.com>
2022-01-29 17:13:28 -0500

    ssl connection for heroku

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,13 @@
 const Sequelize = require('sequelize');
+
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://joe-alves:buttons@localhost:5432/yolga', {
-  logging: false
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: process.env.DATABASE_URL ? true : false,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = db;
